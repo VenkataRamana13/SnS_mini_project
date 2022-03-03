4f3602972f52fcc89422c670523dd9b2a1c5b81e
VenkataRamana13 <76195050+VenkataRamana13@users.noreply.github.com>
2022-03-03 16:28:45 +0530

    code for plotting the ecg file

---
+++ task_2_ecg.m
@@ -0,0 +1,11 @@
+filename = '115.csv';
+Y = readmatrix(filename);
+y = Y(:,2);
+
+idx = 1:3000;
+
+figure()
+plot(y(1:3000,1)); 
+ylabel('y(t)');
+xlabel('t');
+title('The signal read from the csv file');
